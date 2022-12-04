96a6945f5c671bf840fc587ecd27d0e87a6d9d3b
Nicolás <112599449+NicoBogado2022@users.noreply.github.com>
2022-12-04 15:07:56 +0100

    Add files via upload
    Plantilla Bienvenida

---
+++ Bienvenida.java
@@ -0,0 +1,13 @@
+
+package sysacad;
+
+import javax.swing.JOptionPane;
+
+public class Bienvenida {
+    public void Bienvenida(){
+        JOptionPane.showMessageDialog(null, "-----Universidad Tecnológica Nacional-----\n" +
+        "\nFacultad Regional San Rafael\n" +
+        "\nSistema Académico SYSACAD\n" +
+        "\nMódulo de autogestión alumnos");
+    }
+}
